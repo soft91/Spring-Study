970308ebc3a2546b7881021af5cd357a8c2cd14a
soft91 <soft4750@gmail.com>
2020-01-01 02:38:18 +0900

    수정, 삭제 및 예외처리 등록

--- ex01/src/main/java/com/yoon/controller/BoardController.java
+++ ex01/src/main/java/com/yoon/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.yoon.domain.BoardVO;
@@ -42,5 +43,33 @@ public class BoardController {
 	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
 	public void listAll(Model model) throws Exception {
 		logger.info("show all List");
+		model.addAttribute("list", service.listAll());
+	}
+	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception{
+		model.addAttribute(service.read(bno));
+	}
+	
+	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		rttr.addFlashAttribute("msg", "Success");
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public void modifyGET(int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno));
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+		logger.info("mod post..........");
+		
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "Success");
+		
+		return "redirect:/board/listAll";
 	}
 }
--- ex01/src/main/webapp/WEB-INF/views/board/listAll.jsp
+++ ex01/src/main/webapp/WEB-INF/views/board/listAll.jsp
@@ -1,4 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page session = "false" %>
 <%@ include file = "../include/header.jsp" %>
 	<script>
 		var result = '${msg}';
@@ -14,5 +17,14 @@
 			<th>REGDATE</th>
 			<th style = "width:40px">VIEWCNT</th>
 		</tr>
+		<c:forEach items="${list}" var = "boardVO">
+			<tr>
+				<td>${boardVO.bno }</td>
+				<td><a href = "/board/read?bno=${boardVO.bno}">${boardVO.title}</a></td>
+				<td>${boardVO.writer }</td>
+				<td><fmt:formatDate pattern="yyyy-MM-dd HH:mm" value = "${boardVO.regdate }"/></td>
+				<td><span class = "badge bg-red">${boardVO.viewcnt}</span></td>
+			</tr>
+		</c:forEach>
 	</table>
 <%@ include file = "../include/footer.jsp" %>
