287ae5431624dfbc7816c11fe9f3babdd612975b
soft91 <soft4750@gmail.com>
2020-03-19 22:46:29 +0900

    JPA pageable Test, Query Test

--- Chapter05/src/main/java/com/yoon/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/yoon/persistence/BoardRepository.java
@@ -2,11 +2,25 @@ package com.yoon.persistence;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.yoon.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
-	List<Board> findByTitle(String searchKeyword); // 게시글의 제목 검색
-	List<Board> findByContentContaining(String searchKeyword); // 특정 단어가 포함된 목록 검색
+	// 게시글의 제목 검색
+	List<Board> findByTitle(String searchKeyword);
+	// 특정 단어가 포함된 목록 검색
+	List<Board> findByContentContaining(String searchKeyword); 
+	// 제목 또는 내용에 특정 단어가 포함된 글을 조회
+	List<Board> findByTitleContainingOrContentContaining(String title, String content);
+	// 데이터 내림차순 정렬
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	// 1개부터 다섯개의 데이터만 조회
+	Page<Board> findByTitleContaining(String searchKeyword, Pageable paging);
+	// 
+	@Query("SELECT b FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest1(String searchKeyword);
 }
--- Chapter05/src/test/java/com/yoon/QueryMethodTest.java
+++ Chapter05/src/test/java/com/yoon/QueryMethodTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.yoon.domain.Board;
@@ -42,9 +46,47 @@ public class QueryMethodTest {
 //		}
 //	}
 	
+//	@Test
+//	public void testByContentContaining() {
+//		List<Board> boardList = boardRepo.findByContentContaining("17");
+//		
+//		System.out.println("검색 결과");
+//		for(Board board: boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrContentContaining() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrContentContaining("17", "17");
+//		
+//		System.out.println("검색 결과");
+//		for(Board board: boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrderBySeqDesc() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrderBySeqDesc("17");
+//		
+//		System.out.println("검색 결과");
+//		for(Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
 	@Test
-	public void testByContentContaining() {
-		List<Board> boardList = boardRepo.findByContentContaining("17");
+	public void testFindByTitleContaining() {
+		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");
+		Page<Board> pageInfo = boardRepo.findByTitleContaining("제목", paging);
+		
+		System.out.println("PAGE SIZE : " + pageInfo.getSize());
+		System.out.println("TOTAL PAGES : " + pageInfo.getTotalPages());
+		System.out.println("TOTAL COUNT : " + pageInfo.getTotalElements());
+		System.out.println("NEXT : " + pageInfo.nextPageable());
+		
+		List<Board> boardList = pageInfo.getContent();
 		
 		System.out.println("검색 결과");
 		for(Board board: boardList) {
