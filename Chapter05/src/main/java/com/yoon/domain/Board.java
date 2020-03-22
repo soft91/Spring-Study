e859f13a8b0baf1cdffa0dc5646eacdf3791fe75
soft91 <soft4750@gmail.com>
2020-03-22 17:27:33 +0900

    Chapter05 JPA Test

--- Chapter05/src/main/java/com/yoon/domain/Board.java
+++ Chapter05/src/main/java/com/yoon/domain/Board.java
@@ -2,7 +2,13 @@ package com.yoon.domain;
 
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,53 +25,13 @@ public class Board {
 	
 	private Long seq;
 	private String title;
-	private String writer;
+//	private String writer;
 	private String content;
 	@Temporal(value = TemporalType.TIMESTAMP)
 	private Date createDate;
 	private Long cnt;
 	
-
-	public Long getSeq() {
-		return seq;
-	}
-	public void setSeq(Long seq) {
-		this.seq = seq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-	public Long getCnt() {
-		return cnt;
-	}
-	public void setCnt(Long cnt) {
-		this.cnt = cnt;
-	}
-	
-	@Override
-	public String toString() {
-		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" +
-				content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
-	}
+	@ManyToOne
+	@JoinColumn(name="MEMBER_ID", nullable = false)
+	private Member member;
 }
