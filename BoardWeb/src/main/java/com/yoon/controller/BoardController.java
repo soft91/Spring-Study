7bff7dfcbb63c49c0ae777ba6a3625ae36cdb78f
soft91 <soft4750@gmail.com>
2020-04-14 22:51:06 +0900

    타임리프 추가

---
+++ BoardWeb/src/main/java/com/yoon/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.yoon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yoon.domain.Board;
+import com.yoon.service.BoardService;
+
+@Controller
+@RequestMapping("/board/")
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/getBoardList")
+	public String getBoardList(Model model, Board board) {
+		Page<Board> boardList = boardService.getBoardList(board);
+		model.addAttribute("boardList", boardList);
+		return "board/getBoardList";
+	}
+	
+	@GetMapping("/getBoard")
+	public String getBoard(Board board, Model model) {
+		model.addAttribute("board", boardService.getBoard(board));
+		return "board/getBoard";
+	}
+}
